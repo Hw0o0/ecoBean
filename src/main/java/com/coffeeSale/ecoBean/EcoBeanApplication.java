4175334604c519a6ab7c4f575fb930adbf176696
leehyeonwoo <dlgusdn7706@gmail.com>
2023-12-05 14:22:11 +0900

    chore - 프로젝트 생성

---
+++ src/main/java/com/coffeeSale/ecoBean/EcoBeanApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeSale.ecoBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoBeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffeeSale/ecoBean/EcoBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeSale.ecoBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
